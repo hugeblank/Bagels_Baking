b19bb1f61ab6c762bb835ef9fda5df8f6a002525
Todd Lange <toddlangejr@gmail..com>
2021-01-25 19:14:49 -0800

    Bump Mod version & Add textures

--- src/main/java/dev/elexi/hugeblank/bagels_baking/ClientBaking.java
+++ src/main/java/dev/elexi/hugeblank/bagels_baking/ClientBaking.java
@@ -3,7 +3,6 @@ package dev.elexi.hugeblank.bagels_baking;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
-import java.util.ArrayList;
 
 public class ClientBaking implements ClientModInitializer {
 
