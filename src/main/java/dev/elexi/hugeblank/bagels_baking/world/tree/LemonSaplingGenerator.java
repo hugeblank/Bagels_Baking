c780bb9996bf6bc43a904a3a55bff38d394ec602
hugeblank <hugeblank@gmail.com>
2021-08-08 02:06:38 -0700

    Bumping to 1.17.1
    just in dev, prod should still be able to run on 1.17

--- src/main/java/dev/elexi/hugeblank/bagels_baking/world/tree/LemonSaplingGenerator.java
+++ src/main/java/dev/elexi/hugeblank/bagels_baking/world/tree/LemonSaplingGenerator.java
@@ -11,7 +11,7 @@ public class LemonSaplingGenerator extends SaplingGenerator {
 
     public LemonSaplingGenerator() {}
 
-    protected ConfiguredFeature<TreeFeatureConfig, ?> createTreeFeature(Random random, boolean bees) {
+    protected ConfiguredFeature<TreeFeatureConfig, ?> getTreeFeature(Random random, boolean bees) {
         return bees ? BakingConfiguredFeatures.LEMON_TREE_BEES : BakingConfiguredFeatures.LEMON_TREE;
     }
 }
